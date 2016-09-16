6ac5a65c14066aa7d754460e15c88fa1c9919b83
Takuya Matsuyama <nora@odoruinu.net>
2016-09-16 15:57:24 +0900

    Support inline

--- src/index.jsx
+++ src/index.jsx
@@ -8,8 +8,8 @@ export default (languageDefinitions) => {
     Lowlight.registerLanguage(language, definition);
   });
 
-  const Code = ({ className, children }) => (
-    <Lowlight language={className.split('-')[1]} value={children} />
+  const Code = ({ className, children, inline }) => (
+    <Lowlight language={className.split('-')[1]} value={children[0]} inline={ inline } />
   );
   Code.propTypes = {
     className: React.PropTypes.string,
